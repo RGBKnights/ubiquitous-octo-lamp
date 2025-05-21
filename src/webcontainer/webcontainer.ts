17583b0c2ed40340971b7c0987133109a98f0a57
RGBKnights <master@rgbknights.com>
2025-05-21 14:13:41 -0400

    test

--- src/webcontainer/webcontainer.ts
+++ src/webcontainer/webcontainer.ts
@@ -1,13 +1,9 @@
-import { WebContainer, auth } from '@webcontainer/api';
+import { WebContainer } from '@webcontainer/api';
 
 let webcontainerInstance: WebContainer | null = null;
 
 export async function getWebContainer() {
   if (!webcontainerInstance) {    
-    auth.init({
-      clientId: 'wc_api_jamie_maxwell_webster_d5d94a8735574195607c41e7c009f250',
-      scope: '',
-    });
     webcontainerInstance = await WebContainer.boot({coep: 'credentialless' });
   }
   return webcontainerInstance;
