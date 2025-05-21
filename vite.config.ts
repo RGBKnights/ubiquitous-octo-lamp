9608338c4c0bf9eb2cbe0b9675169980faf99504
RGBKnights <master@rgbknights.com>
2025-05-21 13:33:05 -0400

    trying the other one.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
    server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin"
+      "Cross-Origin-Opener-Policy": "cross-origin"
     },
   },
 })
