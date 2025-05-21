c89068b1870d6023a9cef4dd25b4b2b0baa9e40e
RGBKnights <master@rgbknights.com>
2025-05-21 11:08:47 -0400

    changed setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), crossOriginIsolation()],
+  plugins: [vue()],
+   server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
 })
